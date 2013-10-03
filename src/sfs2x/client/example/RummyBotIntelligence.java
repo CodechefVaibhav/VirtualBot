3beb740e5bb9d6d90e96d5c7786fb67b84f514db
CodechefVaibhav <vaibhav.kashyap@hotmail.com>
2013-10-03 17:10:17 +0530

    handling card grouping on initial hand card distribution
    handling card grouping on initial hand card distribution

--- src/sfs2x/client/example/RummyBotIntelligence.java
+++ src/sfs2x/client/example/RummyBotIntelligence.java
@@ -1,6 +1,7 @@
 package sfs2x.client.example;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
@@ -8,6 +9,9 @@ import java.util.Set;
 import java.util.TreeMap;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentMap;
+
+
+//import rummy.CardList;
 
 import common.Card;
 import common.Face;
@@ -18,10 +22,11 @@ public class RummyBotIntelligence {
 	private List<Card> cardlist = null;
 	private List<List<Card>> groupedCards=new ArrayList<List<Card>>();
 	private List<List<Card>> ungroupedCards=new ArrayList<List<Card>>();
-		private List<Card> jokerList = new ArrayList<Card>();
+	private List<Card> jokerList = new ArrayList<Card>();
 	private SuitListClass suitClassObj = null;
 	private SuitListClass[] suitTypeArray = null; 
 	private Card cutJoker = null;
+	private CardList cardListObj = null;
 	//private List
 	
 	public RummyBotIntelligence(RummyBot bot)
@@ -133,10 +138,16 @@ public class RummyBotIntelligence {
 				{
 					suitClassObj.middleOpen.add(cardTemp);
 				}
-				else if(diff == 0)
+				else
 				{
+					/**also covers case
+					 *  else if(diff == 0)
+						{
+							suitClassObj.ungroupedSequesnces.add(cardTemp);
+						}
+					 */
 					suitClassObj.ungroupedSequesnces.add(cardTemp);
-				}	
+				}
 			}
 		}
 		
@@ -158,57 +169,194 @@ public class RummyBotIntelligence {
 					suitClassObj.getOpenEnded().remove(one);
 				}
 			}
+
 		}
 		
-		display(suitClassObj);
+		List<List<Card>> unGroupedListTemp = suitClassObj.getUngroupedSequesnces();
+		for(int k=0; k<unGroupedListTemp.size(); k++)
+		{  
+			List<Card> jokerTempList = unGroupedListTemp.get(k);
+			jokerloop : for(int y=0 ; jokerTempList.size()>0; )
+			{
+				System.out.println(y);
+				Card jokerCheckCard = jokerTempList.get(y);
+				if(isJoker(jokerCheckCard))
+				{
+					jokerList.add(jokerCheckCard);
+					jokerTempList.remove(jokerCheckCard);
+					//continue jokerloop;
+				}
+				else
+				{
+					List<List<Card>> groupedSeqTemp = suitClassObj.getGroupedSequences();
+					for(int i=0 ;i<groupedSeqTemp.size(); i++)
+					{
+						List<Card> c = groupedSeqTemp.get(i);
+						for(int j=0 ; j<c.size(); j++)
+						{
+							Card ctemp = c.get(j);
+							int temp = ctemp.face.value;
+							++temp;
+							if(temp==jokerCheckCard.face.value)
+							{
+								c.add(jokerCheckCard);
+								jokerTempList.remove(jokerCheckCard);
+								sortByFace(c);
+								continue jokerloop;
+								
+							}
+							//System.out.println("open : "+ctemp.face.value +" # "+ctemp.suit.value);
+						}
+					}
+					
+					List<List<Card>> openEndedTemp = suitClassObj.getOpenEnded();
+					for(int i=0; i<openEndedTemp.size() ;i++)
+					{
+						List<Card> c = openEndedTemp.get(i);
+						for(int j=0 ; j<c.size(); j++)
+						{
+							Card ctemp = c.get(j);
+							int temp = ctemp.face.value;
+							++temp;
+							if(temp==jokerCheckCard.face.value)
+							{
+								c.add(jokerCheckCard);
+								sortByFace(c);
+								jokerTempList.remove(jokerCheckCard);
+								if(chechForPure(c))
+								{
+									suitClassObj.getGroupedSequences().add(c);
+									suitClassObj.getOpenEnded().remove(c);
+									continue jokerloop;
+								}		
+							}
+							//System.out.println("open : "+ctemp.face.value +" # "+ctemp.suit.value);
+						}
+					}
+					
+					List<List<Card>> middleOpenTemp = suitClassObj.getMiddleOpen();
+					for(int i=0; i<middleOpenTemp.size() ;i++)
+					{
+						List<Card> c = middleOpenTemp.get(i);
+						for(int j=0; j<c.size(); j++)
+						{
+							Card ctemp = c.get(j);
+							int temp = ctemp.face.value;
+							++temp;
+							if(temp==jokerCheckCard.face.value)
+							{
+								c.add(jokerCheckCard);
+								jokerTempList.remove(jokerCheckCard);
+								c = sortByFace(c);
+								if(checkForPureImpure(c, true))
+								{
+									suitClassObj.getGroupedSequences().add(c);
+									suitClassObj.getMiddleOpen().remove(c);
+									continue jokerloop;
+								}
+								else
+								{
+									c.remove(jokerCheckCard);
+									c = sortByFace(c);
+								}
+							}
+						}
+					}
+				}
+			}
+			if(jokerTempList.isEmpty())
+			{
+				unGroupedListTemp.remove(jokerTempList);
+			}
+		}
+		
+		if(!jokerList.isEmpty())
+		{
+			for(int y=0 ; y<jokerList.size() ; y++)
+			{
+				List<List<Card>> middleOpenToClubedWithJokers = suitClassObj.getMiddleOpen();
+				{
+					for(int j=0; j<middleOpenToClubedWithJokers.size(); j++)
+					{
+						List<Card> middleOpenToClub = middleOpenToClubedWithJokers.get(j);
+						middleOpenToClub.add(jokerList.get(y));
+						middleOpenToClub = sortByFace(middleOpenToClub);
+						if(checkForSequence(middleOpenToClub))
+						{
+							suitClassObj.getGroupedSequences().add(middleOpenToClub);
+							suitClassObj.getMiddleOpen().remove(middleOpenToClub);
+							jokerList.remove(jokerList.get(y));
+						}
+						else
+						{
+							middleOpenToClub.remove(jokerList.get(y));
+							middleOpenToClub = sortByFace(middleOpenToClub);
+						}
+					}
+				}
+			}
+		}
+		
+		//display(suitClassObj);
 			
 	}
 		
 	
-	public void display(SuitListClass suitClassObj)
+	public void display(/*SuitListClass suitClassObj*/)
 	{
-		List<List<Card>> open = suitClassObj.getOpenEnded();
-		for(int i=0 ;i<open.size(); i++)
+		for(int i=0; i<suitTypeArray.length ; i++)
 		{
-			List<Card> c = open.get(i);
-			for(int j=0 ; j<c.size(); j++)
+			List<List<Card>> open = suitTypeArray[i].getOpenEnded();
+			for(int z=0 ;z<open.size(); z++)
 			{
-				Card ctemp = c.get(j);
-				System.out.println(ctemp.face.value +" # "+ctemp.suit.value);
+				List<Card> c = open.get(z);
+				for(int j=0 ; j<c.size(); j++)
+				{
+					Card ctemp = c.get(j);
+					System.out.println("open : "+ctemp.face.value +" # "+ctemp.suit.value);
+				}
 			}
-		}
-		
-		List<List<Card>> group = suitClassObj.getGroupedSequences();
-		for(int i=0 ;i<group.size(); i++)
-		{
-			List<Card> c = group.get(i);
-			for(int j=0 ; j<c.size(); j++)
+			
+			List<List<Card>> group = suitTypeArray[i].getGroupedSequences();
+			for(int x=0 ;x<group.size(); x++)
 			{
-				Card ctemp = c.get(j);
-				System.out.println(ctemp.face.value +" # "+ctemp.suit.value);
+				List<Card> c = group.get(x);
+				for(int j=0 ; j<c.size(); j++)
+				{
+					Card ctemp = c.get(j);
+					System.out.println("group : "+ctemp.face.value +" # "+ctemp.suit.value);
+				}
 			}
-		}
-		
-		List<List<Card>> middle = suitClassObj.getMiddleOpen();
-		for(int i=0 ;i<middle.size(); i++)
-		{
-			List<Card> c = middle.get(i);
-			for(int j=0 ; j<c.size(); j++)
+			
+			List<List<Card>> middle = suitTypeArray[i].getMiddleOpen();
+			for(int v=0 ;v<middle.size(); v++)
 			{
-				Card ctemp = c.get(j);
-				System.out.println(ctemp.face.value +" # "+ctemp.suit.value);
+				List<Card> c = middle.get(v);
+				for(int j=0 ; j<c.size(); j++)
+				{
+					Card ctemp = c.get(j);
+					System.out.println("middle : "+ctemp.face.value +" # "+ctemp.suit.value);
+				}
 			}
-		}
-		
-		List<List<Card>> ungroup = suitClassObj.getUngroupedSequesnces();
-		for(int i=0 ;i<ungroup.size(); i++)
-		{
-			List<Card> c = ungroup.get(i);
-			for(int j=0 ; j<c.size(); j++)
+			
+			List<List<Card>> ungroup = suitTypeArray[i].getUngroupedSequesnces();
+			for(int b=0 ;b<ungroup.size(); b++)
 			{
-				Card ctemp = c.get(j);
-				System.out.println(ctemp.face.value +" # "+ctemp.suit.value);
+				List<Card> c = ungroup.get(b);
+				for(int j=0 ; j<c.size(); j++)
+				{
+					Card ctemp = c.get(j);
+					System.out.println("ungroup : "+ctemp.face.value +" # "+ctemp.suit.value);
+				}
 			}
+			
+			for(int n=0; n<jokerList.size(); n++)
+			{
+				Card ctemp = jokerList.get(n);
+				System.out.println("jokerLIst : "+ctemp.face.value +" # "+ctemp.suit.value);
+			}
+			
+			System.out.println("////////////////////////////////////////////////////////////////////////////////");
 		}
 	}
 			
@@ -242,6 +390,48 @@ public class RummyBotIntelligence {
 //				myList.add(typeNFaceSorted.get(i));
 //				++temp;
 //			}
+	
+	private boolean chechForPure(List<Card> cards)
+	{
+		boolean flag= false;
+		cardListObj = new CardList();
+		cardListObj.list = cards;
+		updateJkrCount(cardListObj);
+		if(isPure(cardListObj))
+		{
+			flag = true;
+		}
+		
+		return flag;
+	}
+	
+	private boolean checkForSequence(List<Card> cards)
+	{
+		boolean flag= false;
+		cardListObj = new CardList();
+		cardListObj.list = cards;
+		updateJkrCount(cardListObj);
+		if(isSeq(cardListObj))
+		{
+			flag = true;
+		}
+		//System.out.println(flag);
+		return flag;
+	}
+	
+	private boolean checkForSet(List<Card> cards)
+	{
+		boolean flag= false;
+		cardListObj = new CardList();
+		cardListObj.list = cards;
+		updateJkrCount(cardListObj);
+		if(isSet(cardListObj))
+		{
+			flag = true;
+		}
+		
+		return flag;
+	}
 	
 	private List<Card> sortByFace(List<Card> cards)
 	{
@@ -294,7 +484,116 @@ public class RummyBotIntelligence {
 	}
 	
 	
+	private boolean duplicateCard(CardList cL, boolean isPure) {
+		//System.out.println("In Duplicate Card "+cL.list.toString()+" ");
+		
+		for (int i = 0; i < cL.length; i++) {
+			for (int j = 0; j < cL.length; j++) {
+				if (i != j && ((isJoker(cL.list.get(i)) == false && isJoker(cL.list.get(j)) == false)||isPure)) 
+				{
+					if (cL.list.get(i).getFace().getValue() == cL.list.get(j).getFace().getValue() &&  
+							cL.list.get(i).getSuit().getValue() == cL.list.get(j).getSuit().getValue() ) {
+						//System.out.println("true");
+						return true; 
+					}
+				}
+			}
+		}
+		//System.out.println("false");
+		return false;
+	}
 	
+	private boolean isSeq(CardList cL) {
+		//System.out.println("\nIn Is Seq");
+		if (cL.list.size() <= 2) {cL.isSeq = false; return false;}
+		if (cL.list.size()-cL.jkrCount <=1) {cL.isSeq = true; return true;}
+		
+		int index = 0;
+		Suit s = cL.list.get(index).getSuit();
+		while (isJoker(cL.list.get(index)) == true && index <cL.list.size()) index++;
+		for (int i = index+1; i < cL.list.size(); i++)
+			if (isJoker(cL.list.get(i)) == false && cL.list.get(i).getSuit().getValue() != s.getValue())
+			{cL.isSeq = false; return false;}
+		
+		if (duplicateCard(cL,false) == true) {cL.isSeq = false; return false;}
+		
+		int[] array = new int[cL.list.size()-cL.jkrCount];
+		int k = 0;
+		for (index = 0; index < cL.list.size(); index++)
+			if (isJoker(cL.list.get(index)) == false) {array[k] = cL.list.get(index).getFace().getValue(); k++;}
+		Arrays.sort(array);//sort in increasing order
+		if (array[array.length-1]-array[0] <= cL.list.size()-1) {cL.isSeq = true; return true;}
+
+		if (array[array.length-1] == 14) {
+			array[array.length-1] = 1;
+			Arrays.sort(array);//sort in increasing order
+			if (array[array.length-1]-array[0] <= cL.list.size()-1) {cL.isSeq = true; return true;}
+		}
+		
+		cL.isSeq = false;
+		return false;
+	}
+	
+	private boolean isSet(CardList cL) {
+		//System.out.println("\nIn Is Set");
+		if (cL.list.size() <= 2) {cL.isSet = false; return false;}
+		if (cL.jkrCount == cL.length) {cL.isSet = true; return true;}
+		
+		int index = 0;
+		while (isJoker(cL.list.get(index)) == true && index <cL.list.size()) index++;
+		Face f = cL.list.get(index).getFace();
+		for (int i = index+1; i < cL.list.size(); i++)
+			if (isJoker(cL.list.get(i)) == false && cL.list.get(i).getFace().getValue() != f.getValue())
+			{cL.isSet = false; return false;}
+
+		if (duplicateCard(cL,false) == true) {cL.isSet = false; return false;}
+		
+		cL.isSet = true;
+		return true;
+	}
+	
+	private boolean isPure(CardList cL) {
+		//System.out.println("\nIn Is Pure");
+		if (cL.list.size() <= 2) {cL.isPure = false; return false;}
+
+		if (cL.paperJkrCount != 0) {cL.isPure = false; return false;}
+		
+		Suit s = cL.list.get(0).getSuit();
+		for (int index = 1; index < cL.list.size(); index++)
+			if (s.getValue() != cL.list.get(index).getSuit().getValue()) {cL.isPure = false; return false;}
+
+		if (duplicateCard(cL,true) == true) {cL.isPure = false; return false;}
+		
+		int[] array = new int[cL.list.size()];
+		for (int k = 0; k < cL.list.size(); k++)
+			{array[k] = cL.list.get(k).getFace().getValue();}
+		Arrays.sort(array);//sort in increasing order
+		if (array[array.length-1]-array[0] == cL.list.size()-1) {cL.isPure = true; return true;}
+
+		if (array[array.length-1] == 14) { 
+			array[array.length-1] = 1;
+			Arrays.sort(array);//sort in increasing order
+			if (array[array.length-1]-array[0] == cL.list.size()-1) {cL.isPure = true; return true;}
+		}
+		
+		cL.isPure = false;
+		return false;
+	}
+	
+	private void updateJkrCount(CardList cL) {
+		//System.out.println("\nIn update jkr count");
+		//cL.length = cL.list.size();
+		for (int index = 0; index <cL.list.size(); index++) {
+			if (cL.list.get(index).getFace().getValue() == getJokerCard().getFace().getValue() || 
+					cL.list.get(index).getFace().getValue() == 21)
+			{
+				cL.jkrCount++; //both cut joker and paper joker
+			}			
+			
+			// paper joker count
+			if (cL.list.get(index).getFace().getValue() == 21) cL.paperJkrCount++;			
+		}
+	}
 	
 	private boolean checkForPureImpure(List<Card> cards, boolean checkForPure)
 	{
@@ -331,41 +630,74 @@ public class RummyBotIntelligence {
 		}
 		
 		boolean isImpurePure = false;
-			if(!checkForPure){
-			faceLoop :for(Card crd :   cardsTemp)
+			if(!checkForPure)
 			{
-				if(firstFace)
-				{   
-					if(isJoker(crd))
+				int nonJokerCardCount = 0;
+				int jokerCardCount =0;
+				for(int z=0 ; z<cardsTemp.size() ; z++)
+				{
+					Card checkNonJoker = cardsTemp.get(z);
+					if(!isJoker(checkNonJoker))
 					{
-						
+						++nonJokerCardCount;
 					}
 					else
 					{
-						tempFaceValue = crd.face.value;
-						tempFaceValue++;
+						++jokerCardCount;
 					}
-					firstFace = false;
 				}
+				if(nonJokerCardCount<=2 && jokerCardCount<=1)
+				{
+					faceLoop :for(Card crd :   cardsTemp)
+					{
+						if(firstFace)
+						{   
+							if(isJoker(crd))
+							{
+								tempFaceValue = -2;
+							}
+							else
+							{
+								tempFaceValue = crd.face.value;
+								tempFaceValue++;
+							}
+							firstFace = false;
+						}
+						else
+						{
+							if(isJoker(crd))
+							{
+								if(tempFaceValue == -2)
+								{
+									tempFaceValue = -3;
+								}
+								else
+								{
+									tempFaceValue = ++tempFaceValue;
+								}
+							}
+							else if(tempFaceValue == -3)
+							{
+								
+							}
+							else if(tempFaceValue == crd.face.value)
+							{
+								System.out.println("isImPure");
+								tempFaceValue = ++tempFaceValue;
+							}
+							else
+							{
+								isImpurePure =  true;
+								break faceLoop;
+							}
+						}
+					}
+			    }
 				else
 				{
-					if(isJoker(crd))
-					{
-						tempFaceValue = ++tempFaceValue;
-					}
-					else if(tempFaceValue == crd.face.value)
-					{
-						System.out.println("isImPure");
-						tempFaceValue = ++tempFaceValue;
-					}
-					else
-					{
-						isImpurePure =  true;
-						break faceLoop;
-					}
+					isImpurePure = true;
 				}
 			}
-		}
 		
 			if(checkForPure)
 			{
@@ -537,7 +869,7 @@ public class RummyBotIntelligence {
 		
 		Face f6=new Face();
 		Suit s6=new Suit();
-		f6.setValue(6);
+		f6.setValue(8);
 		s6.setValue(2);
 		
 		Card c6=new Card();
@@ -547,7 +879,7 @@ public class RummyBotIntelligence {
 		
 		Face f7=new Face();
 		Suit s7=new Suit();
-		f7.setValue(8);
+		f7.setValue(6);
 		s7.setValue(2);
 		
 		Card c7=new Card();
@@ -598,8 +930,19 @@ public class RummyBotIntelligence {
 		c8.setSuit(s8);
 		list.add(c8);
 		
-		rc.onHandCards(list);
 		
+//		Face f9=new Face();
+//		Suit s9=new Suit();
+//		f9.setValue(5);
+//		s9.setValue(0);
+//		
+//		Card c9=new Card();
+//		c9.setFace(f9);
+//		c9.setSuit(s9);
+//		list.add(c9);
+		
+		rc.onHandCards(list);
+		rc.display();
 		list=rc.sortByFace(list);
 		if(rc.isPureSequesnce(list))
 		{
@@ -618,5 +961,18 @@ public class RummyBotIntelligence {
 		{
 			System.out.println("Invalid ");
 		}
+	}
+	
+	class CardList {
+		public List<Card> list = new ArrayList<Card>();
+		public int length = 0;
+		public int jkrCount = 0;
+		public int paperJkrCount = 0;
+		public boolean isSet = false;
+		public boolean isSeq = false;
+		public boolean isPure = false;
+		public boolean isTrinala = false;
+		public int pointsIfValid = 0;
+		public int pointsIfInvalid = 0;
 	}
 }
